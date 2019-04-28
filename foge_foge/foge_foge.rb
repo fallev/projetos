1dcfd4aa0a7680385280c75f04d8597d79bccf15
Fábio Allevato <fallev@hotmail.com>
2019-04-28 18:01:05 -0300

    Criação do jogo foge-foge

---
+++ foge_foge/foge_foge.rb
@@ -0,0 +1,10 @@
+requier_relative 'ui'
+
+def joga (nome)
+
+end
+
+def inicia_fogefoge
+	nome = da_boas_vindas
+	joga nome
+end
---
+++ foge_foge/ui.rb
@@ -0,0 +1,8 @@
+def da_boas_vinda
+	puts "Bem-vindo ao foge-foge"
+	puts "Qual é o seu nome?"
+	nome = gets.strip
+	puts "\n\n\n\n\n\n"
+	puts "Começaremos o jogo para você, #{nome}"
+	nome
+end
