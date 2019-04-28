72108638f7f70c206eedacfe59365d5904c2c34e
Fábio Allevato <fallev@hotmail.com>
2019-04-28 18:35:39 -0300

    Procurando localização do herói

--- foge_foge/foge_foge.rb
+++ foge_foge/foge_foge.rb
@@ -1,7 +1,32 @@
-requier_relative 'ui'
+require_relative 'ui'
+
+def le_mapa (numero)
+	arquivo = "mapa#{numero}.txt"
+	texto = File.read arquivo
+	mapa = texto.split "\n"
+end
+
+def encontra_jogador (mapa)
+	caractere_do_heroi = "H"
+	for linha = 0..(mapa.size-1)
+		linha_atual = mapa[linha]
+		for coluna = 0..(linha_atual.size-1)
+			herois_esta_aqui = linha_atual[coluna] == caractere_do_heroi
+			if herois_esta_aqui
+				#achei linha e coluna
+			end
+		end	
+	end
+	#não achei
+end
 
 def joga (nome)
+	mapa =le_mapa 1
 
+	while true
+		desenha mapa
+		direcao = pede_movimento
+	end
 end
 
 def inicia_fogefoge
--- foge_foge/ui.rb
+++ foge_foge/ui.rb
@@ -1,8 +1,17 @@
-def da_boas_vinda
+def da_boas_vindas
 	puts "Bem-vindo ao foge-foge"
 	puts "Qual é o seu nome?"
 	nome = gets.strip
 	puts "\n\n\n\n\n\n"
 	puts "Começaremos o jogo para você, #{nome}"
 	nome
+end
+
+def desenha (mapa)
+	puts mapa
+end
+
+def pede_movimento
+	puts "para onde deseja ir?"
+	movimento = gets.strip
 end
