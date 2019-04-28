db5672ac5abed748849f231e721a0a6759aad8fc
Fábio Allevato <fallev@hotmail.com>
2019-04-28 20:10:30 -0300

    Alterando a localização do heroi - BUG

--- foge_foge/foge_foge.rb
+++ foge_foge/foge_foge.rb
@@ -39,8 +39,30 @@ def joga (nome)
 		desenha mapa
 		direcao = pede_movimento
 		heroi = encontra_jogador mapa
-		mapa[heroi[0]][heroi[1]] = " "
 		nova_posicao = calcula_nova_posicao heroi, direcao
+
+		if nova_posicao [0] < 0
+			next
+		end
+
+		if nova_posicao [1] < 0
+			next
+		end
+
+		if nova_posicao [0] >= mapa.size
+			next
+		end
+
+		if nova_posicao [1] >= mapa[0].size
+			next
+		end
+
+		if mapa[nova_posicao[0]][nova_posicao[1]] == "X"
+			next
+		end
+
+		mapa[heroi[0]][heroi[1]] = " "
+
 		mapa[nova_posicao[0]][nova_posicao[1]] = "H"
 	end
 end
