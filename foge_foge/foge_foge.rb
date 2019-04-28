be7d2123535a35cb9e0e84144dfbda060f326fb4
Fábio Allevato <fallev@hotmail.com>
2019-04-28 19:11:08 -0300

    Alterando a localização do heroi

--- foge_foge/foge_foge.rb
+++ foge_foge/foge_foge.rb
@@ -11,7 +11,7 @@ def encontra_jogador (mapa)
 	mapa.each_with_index do |linha_atual, linha|
 		coluna_do_heroi = linha_atual.index caractere_do_heroi
 		if coluna_do_heroi != nil
-			return 
+			return [linha, coluna_do_heroi]
 		end
 	end
 	#não achei
@@ -23,6 +23,19 @@ def joga (nome)
 	while true
 		desenha mapa
 		direcao = pede_movimento
+		heroi = encontra_jogador mapa
+		mapa[heroi[0]][heroi[1]] = " "
+		case direcao
+			when "W"
+				heroi[0] -= 1
+			when "S"
+				heroi[0] += 1
+			when "A"
+				heroi[1] -= 1
+			when "D"
+				heroi[1] += 1
+		end
+		mapa[heroi[0]][heroi[1]] = "H"
 	end
 end
 
