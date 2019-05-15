f17d0e3fbf1f7c8fce629707de50d8db2292ec21
Fábio Allevato <fallev@hotmail.com>
2019-05-14 21:11:21 -0300

    primeiro tratamento de bombas - refatorando com recursao para todos os lados

--- foge_foge/heroi.rb
+++ foge_foge/heroi.rb
@@ -19,6 +19,18 @@ class Heroi
     	calcula_nova_posicao "D" 
     end
 
+    def esquerda
+    	calcula_nova_posicao "A" 
+    end
+
+    def baixo
+    	calcula_nova_posicao "S" 
+    end
+
+    def cima
+    	calcula_nova_posicao "W" 
+    end
+
 	def to_array
 		[linha, coluna]
 	end
